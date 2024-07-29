8b8559a4e9acc5b3b17f1e534814d8286aa78a55
Alans <alans.certkojevs@gmail.com>
2024-07-29 23:25:19 +0300

    links

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -14,11 +14,6 @@ import { RouterLink } from '@angular/router';
 export class ProjectsComponent {
   projects: Project[] = [
     { date: '2024-06-12', title: 'SS.LV Auto WebScraper', text: 'A web scraper to automatically extract and update automotive listings from the SS.LV website, capturing details like make, model, year, and price and exports it to CSV.', url: 'ss-lv-auto-webscraper' },
-    { date: '2024-05-15', title: 'E-commerce Platform', text: 'Developed a scalable e-commerce platform for an online retail business, featuring user authentication, product catalog, shopping cart, and order management.', url: '/projects/ecommerce-platform' },
-    { date: '2024-04-10', title: 'Weather App', text: 'Created a weather application using Angular and the OpenWeatherMap API to provide real-time weather updates, forecasts, and weather alerts for multiple cities.', url: '/projects/weather-app' },
-    { date: '2024-03-20', title: 'Chatbot for Customer Service', text: 'Implemented a chatbot using NLP techniques to assist customers with FAQs, order tracking, and support requests on the company website.', url: '/projects/chatbot-customer-service' },
-    { date: '2024-02-28', title: 'Real-Time Data Dashboard', text: 'Built a real-time data dashboard using Angular and WebSocket to visualize live data streams from IoT sensors in a manufacturing plant.', url: '/projects/real-time-data-dashboard' },
-    { date: '2024-01-18', title: 'Online Learning Platform', text: 'Developed an online learning platform with user registration, course management, interactive lessons, and quizzes.', url: '/projects/online-learning-platform' },
   ];
 
 
