6c19e69a0d7b4043283fd531177f28c777331396
Alans <alans.certkojevs@gmail.com>
2024-08-05 18:11:00 +0300

    text change and underline

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,7 +12,7 @@ import { NgFor, NgStyle } from '@angular/common';
 export class HomeComponent {
   projects = [
     {
-      title: 'SS.LV Auto WebScapper',
+      title: 'SS.LV Car WebScapper',
       description: 'A web scraper to automatically extract and update automotive listings from the SS.LV website, capturing details like make, model, year, and price and exports it to CSV.',
       imageUrl: 'assets/sslv.png',
       urlName: 'ss-lv-auto-webscraper'
